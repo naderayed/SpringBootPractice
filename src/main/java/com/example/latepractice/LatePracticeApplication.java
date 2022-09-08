86e107bad04280c9d8bc15612ad52196126a5f1b
NaderAyed <nader.ayed@esprit.tn>
2022-09-08 13:29:27 +0200

    set up and database connection

---
+++ src/main/java/com/example/latepractice/LatePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.latepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/latepractice/LatePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.latepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
